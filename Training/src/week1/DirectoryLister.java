491c63438931cf72c0e6111c37920c062a5c5182
sugam anand <sugamanandymc1@gmail.com>
2016-06-23 05:33:08 +0530

    formatted

--- Training/src/week1/DirectoryLister.java
+++ Training/src/week1/DirectoryLister.java
@@ -5,18 +5,36 @@ import java.io.File;
 public class DirectoryLister {
 
 	public static void main(String[] args) {
-		File curDir = new File(".");
-		getAllFiles(curDir);
+		// path of the directory
+		File curDir = new File("/home/sugam/Downloads/training/personal");
+		//recursively go into sub-folders
+		// level is used for formatting, can be ignored
+		int level=1;
+		getAllFiles(curDir, level);
 
 	}
 
-	private static void getAllFiles(File curDir) {
+	private static void getAllFiles(File curDir, int level) {
+		// get all the files/folders inside current directory
 		File[] filesList = curDir.listFiles();
 		for (File f : filesList) {
-			if (f.isDirectory())
-				getAllFiles(f);
+			if (f.isDirectory()) {
+				for (int i = 1; i <= level; i++) {
+					System.out.print("\t");
+				}
+				System.out.print("[ " + f.getName() + " ]");
+				System.out.println();
+				// if there sub folder then go inside it
+				getAllFiles(f, level + 1);
+			}
+			// print all the files 
 			if (f.isFile()) {
-				System.out.println(f.getName());
+				for (int i = 1; i <= level; i++) {
+					System.out.print("\t");
+				}
+				System.out.print(f.getName());
+				System.out.println();
+
 			}
 		}
 	}
